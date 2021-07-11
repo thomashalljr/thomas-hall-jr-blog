b944d8f1b64e8fbf9c2de793aedf408fc428199a
Thomas Hall Jr <thomashalljunior@gmail.com>
2021-07-11 14:54:58 -0400

    Add CSS to build up site foundation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const munsRubyColor = '#980905'
+
 module.exports = {
   purge: {
     mode: 'production',
@@ -9,14 +11,17 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
-    textColor: {
+    textColor: theme => ({
+      ...theme('colors'),
+      'muns-ruby': munsRubyColor,
       'muns-black': '#050505',
-      'muns-purple': '#68479b',
-    },
+    }),
     fontFamily: {
       'muns-sans': 'Inter,ui-sans-serif,system-ui,-apple-system,Segoe UI,Roboto,Ubuntu,Cantarell,Noto Sans,sans-serif,BlinkMacSystemFont,Helvetica Neue,Arial,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji',
     },
     backgroundColor: theme => ({
+      ...theme('colors'),
+      'muns-ruby': munsRubyColor,
       'muns-gray-100': '#fcf9f9',
     })
   },
