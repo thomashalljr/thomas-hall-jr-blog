ce13c8d6023adceba355947ba9346ca468ac9ba2
Thomas Hall Jr <thomashalljunior@gmail.com>
2022-06-12 14:49:14 -0400

    Replace old webpack require.context w/ import

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,5 +1,4 @@
-import "index.css"
+import "index.css";
 
 // Import all javascript files from src/_components
-const componentsContext = require.context("bridgetownComponents", true, /.js$/)
-componentsContext.keys().forEach(componentsContext)
+import components from "bridgetownComponents/**/*.js";
